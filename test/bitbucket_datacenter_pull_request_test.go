8219dd595be19271040eb9cef2d271d14ec22973
Akshay Pant <akpant@redhat.com>
2026-06-08 19:01:42 +0530

    test: rename bitbucket DC env vars to match main
    Main renamed TEST_BITBUCKET_SERVER_* to
    TEST_BITBUCKET_DATA_CENTER_* but the e2e test code on this
    branch still references the old names, causing all Bitbucket
    Data Center tests to fail with missing env var errors under
    pull_request_target.
    
    Signed-off-by: Akshay Pant <akpant@redhat.com>

--- test/bitbucket_datacenter_pull_request_test.go
+++ test/bitbucket_datacenter_pull_request_test.go
@@ -20,7 +20,7 @@ func TestBitbucketDataCenterPullRequest(t *testing.T) {
 	t.Skip("skipping test for now, until we ge the bitbucket infra back")
 	targetNS := names.SimpleNameGenerator.RestrictLengthWithRandomSuffix("pac-e2e-ns")
 	ctx := context.Background()
-	bitbucketWSOwner := os.Getenv("TEST_BITBUCKET_SERVER_E2E_REPOSITORY")
+	bitbucketWSOwner := os.Getenv("TEST_BITBUCKET_DATA_CENTER_E2E_REPOSITORY")
 
 	ctx, runcnx, opts, client, err := tbbdc.Setup(ctx)
 	assert.NilError(t, err)
@@ -52,7 +52,7 @@ func TestBitbucketDataCenterCELPathChangeInPullRequest(t *testing.T) {
 	t.Skip("skipping test for now, until we ge the bitbucket infra back")
 	targetNS := names.SimpleNameGenerator.RestrictLengthWithRandomSuffix("pac-e2e-ns")
 	ctx := context.Background()
-	bitbucketWSOwner := os.Getenv("TEST_BITBUCKET_SERVER_E2E_REPOSITORY")
+	bitbucketWSOwner := os.Getenv("TEST_BITBUCKET_DATA_CENTER_E2E_REPOSITORY")
 
 	ctx, runcnx, opts, client, err := tbbdc.Setup(ctx)
 	assert.NilError(t, err)
